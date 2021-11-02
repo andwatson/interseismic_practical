c9e9bffdfd3a936b8a2ffb617be2009148711c3d
Andrew Watson <eearw@leeds.ac.uk>
2021-11-02 18:10:41 +0000

    Updates to practical

--- interseis_lib.py
+++ interseis_lib.py
@@ -14,18 +14,19 @@ import matplotlib.path as path
 
 #-------------------------------------------------------------------------------
 
-def screw_disc(x, s, d, c):
+def screw_disc(x, s, d, c, xc = 0):
     '''
     Function to calculate displacements/velocities due to slip on a deep 
     screw dislocation (infinitely long strike slip fault. 
     After Savage and Burford (1973).
-    v = (s/pi)*arctan(x/d)
+    v = (s/pi)*arctan((x+xc)/d)
 
     INPUTS
         x = vector of distances from fault
         s = slip or slip rate on deep dislocation
         d = locking depth [same units as x]
         c = scalar offset in y [same unit as s]
+        xc = offset to fault location [same units as x]
     OUTPUTS
         v = vector of displacements or velocities at locations defined by x
           [same units as s]
@@ -39,6 +40,64 @@ def screw_disc(x, s, d, c):
     
     return v
 
+#-------------------------------------------------------------------------------
+
+def fault_creep(x, s1, s2, d1, d2, c, xc=0):
+    '''
+    Model for interseismic strain accumulation and fault creep.
+    Taken from Hussain et al. (2016).
+
+    INPUTS
+        x = vector of distances from fault
+        s1 = slip or slip rate on deep dislocation
+        s2 = creep or creep rate
+        d1 = locking depth
+        d2 = depth to bottom of creeping section
+        c = scalar offset in y [same unit as s]
+        xc = offset of fault location
+    OUTPUTS
+        v = vector of displacements or velocities at locations defined by x
+          [same units as s]
+
+    '''
+    
+    
+    v = (s1/np.pi)*np.arctan((x+xc)/d1) + c - s2*((1/np.pi)*np.arctan((x+xc)/d2) + (x<=0)*0.5 - (x>0)*0.5)
+    
+    
+    
+    #(m(1)/pi)*atan(x./m(2)) + m(3) - m(4)*((1/pi)*atan(x./m(5)) + (x<=0)*1/2 - (x>0)*1/2);
+    
+    return v
+
+#-------------------------------------------------------------------------------
+
+def gen_inc(inc_min, inc_max, heading, x, y):
+    
+    #d = (np.amax(x) - np.amin(x)) / np.cos(np.deg2rad(heading))
+    
+    #m = (inc_max - inc_min) / d
+    
+    if (0 <= heading < 45) or (315 <= heading <= 360):
+        d = (np.amax(x) - np.amin(x)) / np.cos(np.deg2rad(heading))
+    elif (45 <= heading < 135):
+        d = (np.amax(y) - np.amin(y)) / np.sin(np.deg2rad(heading))
+    elif (135 <= heading < 225):
+        d = - (np.amax(x) - np.amin(x)) / np.cos(np.deg2rad(heading))
+    elif (225 <= heading < 315):
+        d = - (np.amax(y) - np.amin(y)) / np.sin(np.deg2rad(heading))
+    
+    m = (inc_max-inc_min) / d
+    #m = (inc_max-inc_min) / np.sqrt( (np.amax(x) - np.amin(x))**2 + (np.amax(y) - np.amin(y))**2 )
+    
+    mx = m * np.cos(np.deg2rad(heading))
+    my = m * np.sin(np.deg2rad(heading))
+    
+    xx, yy = np.meshgrid(x,y)
+    
+    inc_grid = mx*xx + my*yy + (inc_min + (inc_max-inc_min)/2)
+    
+    return inc_grid
 
 #-------------------------------------------------------------------------------
 
@@ -53,10 +112,8 @@ def loglike(x, v, m, W):
         ll = value of the loglikelihood function
     '''
     
-    m = m * np.array([0.001, 1000, 0.001])
-    
     #ll = np.sum((np.transpose(v-screw_disc(x, m[0], m[1], m[2]))*W*(v-screw_disc(x, m[0], m[1], m[2]))));
-    ll = np.sum(0.001*(np.transpose(v-screw_disc(x, m[0], m[1], m[2]))*W*(v-screw_disc(x, m[0], m[1], m[2]))));
+    ll = np.sum(-0.01*(np.transpose(v-screw_disc(x, m[0], m[1], m[2]))*W*(v-screw_disc(x, m[0], m[1], m[2]))));
     
     return ll
 
@@ -90,6 +147,64 @@ def rms_misfit(a,b):
     
     return rms
 
+#-------------------------------------------------------------------------------
+'''
+def run_inversion(n_iterations=10000):
+    ''
+    Bayesian monte carlo inversion taken from the main notebook.
+    Re-packaged as a function here to minimise code repeats.
+
+    INPUTS
+        x = vector of distances from fault
+        s = slip or slip rate on deep dislocation
+        d = locking depth [same units as x]
+        c = scalar offset in y [same unit as s]
+        xc = offset of fault location
+    OUTPUTS
+        v = vector of displacements or velocities at locations defined by x
+          [same units as s]
+    ''
+# run inversion
+for ii in range(n_iterations):
+    
+    # propose model using different step sizes for each parameter
+    m_trial = m_current.copy()
+    m_trial[0] = m_trial[0] + np.random.normal(loc=0, scale=2.5, size=1)/1000 # slip rate
+    m_trial[1] = m_trial[1] + np.random.normal(loc=0, scale=2.5, size=1)*1000 # locking depth
+    m_trial[2] = m_trial[2] + np.random.normal(loc=0, scale=1, size=1)/1000 # offset
+    
+    # check limits and skip the rest of the loop if any parameter is invalid
+    if not(lib.logprior(m_trial,m_min,m_max)):
+        n_reject += 1
+        models_saved[ii,:] = m_current
+        continue
+    
+    # calculate likelihood for the current and trial models
+    ll_current = lib.loglike(x, v, m_current, W)
+    ll_trial = lib.loglike(x, v, m_trial, W)
+    
+    #print(np.exp(ll_trial-ll_current))
+    
+    # test whether to keep trial model
+    #if np.exp(ll_current-ll_trial) > np.random.uniform(low=0, high=1,size=1):
+    if np.exp(ll_trial-ll_current) > np.random.uniform(low=0, high=1,size=1):
+    #if (ll_trial < ll_current) or (np.random.uniform(low=0, high=1,size=1) > 0.75):
+        m_current = m_trial
+        ll_current = ll_trial
+        n_accept += 1
+    else:
+        n_reject += 1
+    models_saved[ii,:] = m_current
+    ll_saved[ii] = ll_current
+    
+# convert back from metres to mm/yr and km
+models_saved[:,0] = models_saved[:,0] * 1000 # slip rate
+models_saved[:,1] = models_saved[:,1] / 1000 # locking depth
+models_saved[:,2] = models_saved[:,2] * 1000 # offset
+
+# find best fit model using min of likelihood function
+best_model = models_saved[np.nanargmax(ll_saved),:]
+'''
 #-------------------------------------------------------------------------------
 
 def get_par(par_file,par_name):
@@ -191,3 +306,54 @@ def profile_data(x,y,data,prof_start,prof_end,params):
     points_dist = points2prof_start * np.cos(points_prof_angle)
     
     return bin_val, prof_bin_mids, points_val, points_dist, points_poly
+
+#-------------------------------------------------------------------------------
+
+def profile_fault_intersection(prof_start,prof_end,fault_trace):
+    '''
+    Calculates the distance along a profile at which it intersects a fault, and 
+    the angle between the two at this point.
+    
+    INPUTS:
+        prof_start = (x,y) coord
+        prof_end = (x,y) coords
+        fault_trace = x and y coords of fault trace
+        
+    OUTPUTS:
+        intersection_distance
+        intersection_angle
+    '''
+    
+    # loop through all fault segments to find intersection
+    for ind in range(fault_trace.shape[0]-1):
+        if intersect(prof_start,prof_end,fault_trace[ind,:],fault_trace[ind+1,:]):
+            inter_ind = ind
+            break
+    
+    # get coords for either side of intersection segment
+    fault_inter_coords = fault_trace[inter_ind:inter_ind+2,:]
+    
+    # calc gradient of of profile line and fault segment
+    prof_m = (prof_start[1] - prof_end[1]) / (prof_start[0] - prof_end[0])
+    fault_m = (fault_inter_coords[0,1] - fault_inter_coords[1,1]) / (fault_inter_coords[0,0] - fault_inter_coords[1,0])
+    
+    # calculate intersection angle
+    intersection_angle = np.arctan((fault_m - prof_m) / (1 + prof_m * fault_m));
+    
+    # calculate distance to intersection point (from https://stackoverflow.com/questions/3252194/numpy-and-line-intersections)
+    s = np.vstack([prof_start,prof_end,fault_inter_coords[0,:],fault_inter_coords[1,:]])        # s for stacked
+    h = np.hstack((s, np.ones((4, 1)))) # h for homogeneous
+    l1 = np.cross(h[0], h[1])           # get first line
+    l2 = np.cross(h[2], h[3])           # get second line
+    x, y, z = np.cross(l1, l2)          # point of intersection
+    intersection_point = np.array([x/z, y/z])
+    intersection_distance = np.sqrt((intersection_point[0]-prof_start[0])**2 + (intersection_point[1]-prof_start[1])**2);    
+    
+    return intersection_distance, np.rad2deg(intersection_angle)
+
+
+def ccw(A,B,C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
